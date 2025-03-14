761ef20b3e63c4fe6c9888a102e15750afcb42cb
jaideepkhaira102 <jaideepkhairab00102@gmail.com>
2025-03-14 01:39:38 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Base website loaded!");
+});
+
+function toggleTheme() {
+    document.body.classList.toggle("dark-theme");
+}
