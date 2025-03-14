f28d45bfac3e84809fa3e393a1f16bf5371c0cad
jaideepkhaira102 <jaideepkhairab00102@gmail.com>
2025-03-14 11:15:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ document.addEventListener("DOMContentLoaded", function() {
 function toggleTheme() {
     document.body.classList.toggle("dark-theme");
 }
+console.log("Navbar loaded!");
+var numberofsections = 4;
+const sections = ["Home", "About", "Services", "Contact", ];
+document.addEventListener("DOMContentLoaded", function() {
+    // Create Navbar HTML as a string
+    
+    var navbarHTML = `
+        <nav class="navbar">
+            <div class="logo">My Website</div>
+            <button class="menu-toggle" onclick="toggleMenu()">☰</button>
+            <ul class="nav-links">` ;
+
+    // Inject Navbar into the page
+    for(var i=0; i<numberofsections; i++){
+        var navbarHTML = navbarHTML+`<li><a href="#`+sections[i]+`">`+sections[i]+`</a></li>`
+    };
+    var navbarHTML = navbarHTML+`
+            </ul>
+        </nav>
+    `;
+    document.getElementById("navbar-container").innerHTML = navbarHTML;
+});
+
+
+
+function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('active');
+}
